2acb6dd86086461624ba539750feaba88363f87a
Anna <anna.anife@gmail.com>
2023-03-23 18:41:51 +0100

    Added Task19 (hard)

---
+++ DZ-3/Task19_hard/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход целое число любой разрядности и проверяет, 
+// является ли оно палиндромом. Через строку нельзя решать само собой.
+
+
+Console.WriteLine("Введите любое положительное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int temp = num;
+int sum = 0;
+int reminder = 0;
+
+
+CheckPalyndrom(num, temp, sum, reminder);
+
+
+
+
+void CheckPalyndrom(int num, int temp, int sum, int reminder)
+{
+    if (num > 0)
+    {
+        while (num > 0)
+        {
+            reminder = num % 10;
+            sum = (sum * 10) + reminder; 
+            num = num / 10;
+        }
+        if (temp == sum)
+        {
+            Console.WriteLine("Это Палиндром");
+        }
+        else
+        {
+            Console.WriteLine("Это НЕ палидром");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Это НЕ положительное число");
+    }
+}
