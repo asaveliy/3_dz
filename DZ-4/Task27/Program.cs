78c2f88bc654a447d4a38fc0e0419d541a321589
Anna <anna.anife@gmail.com>
2023-03-23 18:56:43 +0100

    Added Task27

---
+++ DZ-4/Task27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.WriteLine("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(num);
+int length = str.Length;
+
+
+
+int Summa(int sum, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+int sum = Summa(0, length);
+Console.WriteLine(sum);
+
+
+
