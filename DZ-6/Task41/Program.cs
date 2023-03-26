309bc93b6b241c59fabfabe1b2bcac6b28f20374
Anna <anna.anife@gmail.com>
2023-03-26 17:18:19 +0200

    Added Task 41

---
+++ DZ-6/Task41/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 5
+// 0, 7, 8, -2, -2 -> 2
+// 5
+// 1, -7, 567, 89, 223-> 3
+
+
+
+int[] FormArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write("Введите число и нажмите [Enter]:");
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+    return arr;
+}
+
+
+
+int CountPos(int[] arr)
+{
+    int count = 0;
+    foreach (int item in arr)
+    {
+        if (item > 0) count += 1;
+    }
+    return count;
+}
+
+
+
+Console.WriteLine("Сколько цифр желаете ввести?");
+int length = int.Parse(Console.ReadLine());
+int[] array = new int[length];
+FormArray(array);
+Console.WriteLine($"Введено {CountPos(array)} четных чисел.");
+
+
