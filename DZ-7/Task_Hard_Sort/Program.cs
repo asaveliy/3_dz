1776703587a1783f1adab4283c0ac2a298b21466
Anna <anna.anife@gmail.com>
2023-04-08 13:27:12 +0200

    updated (2) Hard Sort

--- DZ-7/Task_Hard_Sort/Program.cs
+++ DZ-7/Task_Hard_Sort/Program.cs
@@ -11,56 +11,68 @@
 
 void FillArray(int[,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-        for (int j = 0; j < array.GetLength(1); j++)
-            array[i, j] = new Random().Next(0, 20);
+    for (int zapuscols = 0; zapuscols < array.GetLength(0); zapuscols++)
+        for (int rows = 0; rows < array.GetLength(1); rows++)
+            array[zapuscols, rows] = new Random().Next(0, 20);
 }
-
 
 
 
 int[,] SortArray(int[,] arr)
 {
     int temp = arr[0, 0];
-    for (int i = 0; i < arr.GetLength(1); i++)  // 3. повторно запускает (2) и следовательно (1) до победного
-        for (int j = 0; j < arr.GetLength(0); j++) // 2. переключает строки и снова запускает (1) 
-            for (int k = 1; k < arr.GetLength(1); k++) // 1. запускает проверку по строке
-                if (temp > arr[j, k])
+    int x = 0;
+    for (int y = 1; y < arr.GetLength(1); y++)
+    {
+        while (arr[x, y - 1] > arr[x, y])
+        {
+            for (int i = 0; i <= arr.GetLength(0); i++)
+                for (int j = 0; j <= arr.GetLength(1); j++)
                 {
-                    temp = arr[j, k - 1];
-                    arr[j, k - 1] = arr[j, k];
-                    arr[j, k] = temp;
+                    temp = arr[i, j];
+                    arr[i, j] = arr[i, j + 1];
+                    arr[i, j + 1] = temp;
                 }
-                else
-                {
-                    temp = arr[j, k - 1];
-                }
-    temp = arr[0, 0];
-    for (int i = 0; i < arr.GetLength(0); i++)  // 3. запускаем (2) и (1) до победного
-        for (int j = 1; j < arr.GetLength(1); j++)  // 2. перебираем столбцы
-            for (int k = 0; k < arr.GetLength(0); k++) // 1. перебираем столбцы и больший эл. передвигаем вниз
-                if (temp > arr[j, k])
-                {
-                    temp = arr[j - 1, k];
-                    arr[j - 1, k] = arr[j, k];
-                    arr[j, k] = temp;
-                }
-                else
-                {
-                    temp = arr[j - 1, k];
-                }
+        }
+    }
     return arr;
 }
+
+
+// int[,] SortArray(int[,] arr)
+// {
+//     for (int zap = 0; zap < arr.GetLength(0); zap++)  // 3. повторно запускает (2) и следовательно (1) до победного 
+//         for (int rows = 0; rows < arr.GetLength(1); rows++) // 2. переключает строки и снова запускает (1)  
+//             for (int cols = rows + 1; cols < arr.GetLength(1); cols++) // 1. запускает проверку по строке 
+//                 if (arr[zap, cols] < arr[zap, rows])
+//                 {
+//                     int temp = arr[zap, cols];
+//                     arr[zap, cols] = arr[zap, rows];
+//                     arr[zap, rows] = temp;
+//                 }
+//     return arr;
+// }
+
+
+// for (int zap = 0; zap < arr.GetLength(1); zap++)
+//         for (int cols = 0; cols < arr.GetLength(0); cols++)
+//             for (int rows = cols + 1; rows < arr.GetLength(0); rows++)
+//                 if (arr[zap, rows] < arr[zap, cols])
+//                 {
+//                     int temp = arr[zap, rows];
+//                     arr[zap, rows] = arr[zap, cols];
+//                     arr[zap, cols] = temp;
+//                 }
 
 
 
 
 void PrintArray(int[,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int zapuscols = 0; zapuscols < array.GetLength(0); zapuscols++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-            Console.Write($"{array[i, j],3} \t");
+        for (int rows = 0; rows < array.GetLength(1); rows++)
+            Console.Write($"{array[zapuscols, rows],3} \t");
         Console.WriteLine();
     }
 }
